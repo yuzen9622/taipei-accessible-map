9e075f08042c518a796579b3dd1967a7d040341c
yuzen9622 <oscar48079@gmail.com>
2025-10-01 18:24:46 +0800

    refactor: 重構InfoDrawer與LoadingDrawer組件，優化導入與結構

--- src/components/shared/LoadingDrawer.tsx
+++ src/components/shared/LoadingDrawer.tsx
@@ -1,11 +1,13 @@
 import { Loader2Icon } from "lucide-react";
 
-import { DrawerTitle } from "../ui/drawer";
+import { DrawerContent, DrawerTitle } from "../ui/drawer";
 
 export default function LoadingDrawer() {
   return (
-    <DrawerTitle className="w-full h-full flex items-center justify-center">
-      <Loader2Icon className="animate-spin" />
-    </DrawerTitle>
+    <DrawerContent>
+      <DrawerTitle className="w-full h-full flex items-center justify-center">
+        <Loader2Icon className="animate-spin" />
+      </DrawerTitle>
+    </DrawerContent>
   );
 }
