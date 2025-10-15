23f1935547b0a1bb38a8da9feca58aecb87b2eb3
yuzen9622 <oscar48079@gmail.com>
2025-10-16 00:05:13 +0800

    feat: 更新 package.json 和 package-lock.json，修改專案名稱及版本號，調整 postcss.config.mjs 配置格式

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-/** @type {import('postcss').ProcessOptions} */
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
