2495a1bd81cd3ed0c592bd17df62d976a20aa424
yuzen9622 <oscar48079@gmail.com>
2025-10-15 23:57:44 +0800

    feat: 更新 postcss.config.mjs，調整插件配置格式以支援 tailwindcss 和 autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+/** @type {import('postcss').ProcessOptions} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
