0c8b04dab4b0e05700298744d7c9d60bd3ed2f04
yuzen9622 <oscar48079@gmail.com>
2026-07-19 20:58:32 +0800

    build(next): revert output mode to standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   /* config options here */trailingSlash: true,
 
   images: { unoptimized: true,
